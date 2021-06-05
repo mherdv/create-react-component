883894aadef014a13d2a3626d05fa31bc8f4bef5
Mher <mherdv@gmail.com>
2021-06-05 12:12:55 +0400

    first commit

---
+++ bin/getComponents.js
@@ -0,0 +1,35 @@
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+module.exports.getComponentName = (name)=>{
+    const newName = name.split('-').map(namePart=>{
+        return capitalizeFirstLetter(namePart)
+    }).join('')
+
+    return newName
+}
+
+
+module.exports.getFunctionalComponent = (newName,hooks)=> {
+
+    const component = `import React from 'react';
+import { use${newName} } from './use${newName}'
+    
+const ${newName} = ()=>{
+    ${hooks? `const hooks = use${newName}()`:''}
+    return <>${newName}</>
+}
+
+export default ${newName}`;
+
+    return component
+}
+
+module.exports.getFunctionalComponentCustomHook = (name)=>{
+    const component = `export const ${name} = ()=>{
+    return {}
+}`
+
+    return component
+}
---
+++ bin/index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs-extra');
+const {getFunctionalComponent,getComponentName,getFunctionalComponentCustomHook} = require('./getComponents')
+
+let componentName;
+
+const program = require('commander')
+    .version('0.1.6')
+    .arguments('<component-directory>')
+    .action( (name)=> {
+        componentName = name;
+    })
+    .option('-h, --hooks', 'Create Function with hooks')
+    // .option('-c, --css', `Add ${componentName}.css`)
+    // eslint-disable-next-line no-undef
+    .parse(process.argv)
+
+createComponent(componentName);
+
+function createComponent(name) {
+    const root = path.resolve(name);
+    const options = program._optionValues
+
+    console.log({options})
+
+    if (!fs.existsSync(root)) {
+        fs.mkdirSync(root);
+    }
+
+    const componentName = getComponentName(name);
+
+    fs.writeFileSync(
+        path.join(root, `${componentName}.jsx`),
+        getFunctionalComponent(componentName,options.hooks)
+    )
+    fs.writeFileSync(
+        path.join(root, `use${componentName}.js`),
+        getFunctionalComponentCustomHook(`use${componentName}`)
+    )
+
+}
